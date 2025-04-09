06fd8f1bb806eeff57555fc862dc7ea1d0d10d48
llreebor <timoschuk.roman@gmail.com>
2025-04-09 14:52:55 +0300

    add new sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-function burgerMenu() {
+// Mobile Menu
+function toggleMobileMenu() {
 	const menu = document.querySelector("#mobile-menu")
 	const burger = document.querySelector("#burger")
 	const body = document.querySelector("body")
@@ -26,18 +27,32 @@ function burgerMenu() {
 		}
 	})
 }
-function toggleSubmenu() {
-	const submenuTriggers = document.querySelectorAll(".with-submenu")
+toggleMobileMenu()
 
-	submenuTriggers.forEach((trigger) => {
-		trigger.addEventListener("click", (e) => {
-			if (e.target.classList.contains("submenu-trigger")) {
-				trigger.classList.toggle("active")
+function toggleAccordion() {
+	const items = document.querySelectorAll(".accordion-item")
+	const triggers = document.querySelectorAll(".accordion-trigger")
+	const contents = document.querySelectorAll(".accordion-content")
+	triggers.forEach((trigger) => {
+		if (items[0].classList.contains("active")) {
+			contents[0].classList.add("active")
+		}
+		trigger.addEventListener("click", () => {
+			const parent = trigger.parentNode
+			if (!parent.classList.contains("active")) {
+				// If you want only one to be open at a time, and others to close - UNCOMMENT the code below.
+				// items.forEach((item, i) => {
+				// 	if (i !== idx && item.classList.contains('active')) {
+				// 		item.classList.remove('active')
+				// 	}
+				// })
+				// Open the current accordion item
+				parent.classList.add("active")
+			} else {
+				// Close the current accordion item
+				parent.classList.remove("active")
 			}
 		})
 	})
 }
-if (document.getElementById("mobile-menu")) {
-	burgerMenu()
-	toggleSubmenu()
-}
+toggleAccordion()
